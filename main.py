3a1ab9193a91c2ab38a02bb6f5320039322e8d39
tharusha dilhara jayadeera <87853645+tharusha-dilhara-jayadeera@users.noreply.github.com>
2022-02-24 17:42:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import telebot
 
-bot = telebot.TeleBot("api key")
+bot = telebot.TeleBot("5143362253:AAH28kD97_ygPePbOJr7n8mSZHxNQ8WDyh0")
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
